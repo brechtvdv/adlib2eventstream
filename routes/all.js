19901748f105cbc814f4a4cd13748e449aaf38b9
brechtvdv <br5cht@hotmail.com>
2020-11-26 16:41:47 +0100

    Fix redirect for dataset URIs

--- routes/all.js
+++ routes/all.js
@@ -3,6 +3,13 @@ let { getEventstream } = require("../controllers/eventstreamController");
 let { getDiscoveryMetadata } = require("../controllers/discoveryController");
 let { getSubjectPageOfMember } = require("../controllers/subjectPageController");
 
+let Config = require("../config/config.js");
+let config = Config.getConfig();
+let port = config.eventstream.port != '' ? ':' + config.eventstream.port : '';
+let path = config.eventstream.path != '' ? config.eventstream.path + '/' : '';
+
+const baseURI = config.eventstream.protocol + '://' + config.eventstream.hostname + port + '/' + path;
+
 const router = express.Router();
 
 router.get('/favicon.ico', (req, res) => res.status(404).send("Not found"));
@@ -10,8 +17,8 @@ router.get('/favicon.ico', (req, res) => res.status(404).send("Not found"));
 router.get('/', getDiscoveryMetadata)
 router.get("/:institution/:adlibDatabase", getEventstream);
 
-router.get('/id/datasetcatalogus/:ref', (req, res) => res.redirect('/'));
-router.get('/:institution/id/dataset/:ref', (req, res) => res.redirect('/'))
+router.get('/id/datasetcatalogus/:ref', (req, res) => res.redirect(baseURI));
+router.get('/:institution/id/dataset/:ref', (req, res) => res.redirect(baseURI))
 router.get('/:institution/id/:database/:refBasis/:refVersie', getSubjectPageOfMember);
 
 module.exports = router;
