c0567d0c20ec1d2df35635de3de53d0a0c687894
brechtvdv <br5cht@hotmail.com>
2021-03-30 17:20:00 +0200

    Fix #11, #12

--- bin/adlib2backend.js
+++ bin/adlib2backend.js
@@ -3,6 +3,9 @@ let ObjectMapper = require('../lib/mappers/objectMapper.js');
 let DmgMapper = require('../lib/mappers/dmgMapper');
 let StamMapper = require('../lib/mappers/stamMapper');
 const HvAMapper = require("../lib/mappers/hvaMapper");
+const ArchiefGentMapper = require("../lib/mappers/archiefGentMapper");
+const IndustriemuseumMapper = require("../lib/mappers/IndustriemuseumMapper");
+const ThesaurusMapper = require("../lib/mappers/thesaurusMapper");
 const Backend = require("../lib/Backend");
 const Utils = require('../lib/utils.js');
 const config = require("../config/config.js").getConfig();
@@ -25,19 +28,24 @@ async function start() {
 
     startHva();
     startDmg();
-    //startIndustriemuseum();
-   //startArchiefgent();
+    startIndustriemuseum();
+    //startArchiefgent();
     startStam();
+
+    startThesaurus();
+    startPersonen();
 }
 
-async function startHva() {
+function startHva() {
     let options = {
         "institution": "hva", // to retrieve name and URI from config
         "adlibDatabase": "objecten",
-        "db": sequelize
+        "db": sequelize,
+        "checkEuropeanaFlag": true
     };
     const backend = new Backend(options);
     let objectAdlib = new Adlib(options);
+    options["adlib"] = objectAdlib;
     let objectMapper = new HvAMapper(options);
     objectAdlib.getStream().pipe(objectMapper).pipe(backend);
 }
@@ -46,11 +54,13 @@ function startDmg() {
     let options = {
         "institution": "dmg", // to retrieve name and URI from config
         "adlibDatabase": "objecten",
-        "db": sequelize
+        "db": sequelize,
+        "checkEuropeanaFlag": true
     };
     // Create eventstream "objects" of Design Museum Ghent
     const backend = new Backend(options);
     let objectAdlib = new Adlib(options);
+    options["adlib"] = objectAdlib;
     let objectMapper = new DmgMapper(options);
     objectAdlib.getStream().pipe(objectMapper).pipe(backend);
 }
@@ -59,13 +69,14 @@ function startIndustriemuseum() {
     let options = {
         "institution": "industriemuseum", // to retrieve name and URI from config
         "adlibDatabase": "objecten",
-        "db": sequelize
+        "db": sequelize,
+        "checkEuropeanaFlag": true
     };
     // Create eventstream "personen" of Industriemuseum
     const backend = new Backend(options);
     let objectAdlib = new Adlib(options);
-    let objectMapper = new ObjectMapper(options);
-    let objectSqliteBackend = new SqliteBackend(options);
+    options["adlib"] = objectAdlib;
+    let objectMapper = new IndustriemuseumMapper(options);
     objectAdlib.getStream().pipe(objectMapper).pipe(backend);
 }
 
@@ -73,12 +84,14 @@ function startArchiefgent() {
     let options = {
         "institution": "archiefgent", // to retrieve name and URI from config
         "adlibDatabase": "objecten",
-        "db": sequelize
+        "db": sequelize,
+        "checkEuropeanaFlag": true
     };
     // Create eventstream "personen" of Archief Gent
     const backend = new Backend(options);
     let objectAdlib = new Adlib(options);
-    let objectMapper = new ObjectMapper(options);
+    options["adlib"] = objectAdlib;
+    let objectMapper = new ArchiefGentMapper(options);
     objectAdlib.getStream().pipe(objectMapper).pipe(backend);
 }
 
@@ -86,11 +99,43 @@ function startStam() {
     let options = {
         "institution": "stam", // to retrieve name and URI from config
         "adlibDatabase": "objecten",
-        "db": sequelize
+        "db": sequelize,
+        "checkEuropeanaFlag": true
     };
     // Create eventstream "personen" of Stam
     const backend = new Backend(options);
     let objectAdlib = new Adlib(options);
+    options["adlib"] = objectAdlib;
     let objectMapper = new StamMapper(options);
     objectAdlib.getStream().pipe(objectMapper).pipe(backend);
+}
+
+function startThesaurus() {
+    let options = {
+        "institution": "adlib", // one thesaurus for all institutions
+        "adlibDatabase": "thesaurus",
+        "type": "concept",
+        "db": sequelize,
+        "checkEuropeanaFlag": false
+    };
+    const backend = new Backend(options);
+    const objectAdlib = new Adlib(options);
+    options["adlib"] = objectAdlib;
+    const thesaurusMapper = new ThesaurusMapper(options);
+    objectAdlib.getStream().pipe(thesaurusMapper).pipe(backend);
+}
+
+function startPersonen() {
+    let options = {
+        "institution": "adlib", // one thesaurus for all institutions
+        "adlibDatabase": "personen",
+        "type": "entiteit",
+        "db": sequelize,
+        "checkEuropeanaFlag": false
+    };
+    const backend = new Backend(options);
+    const objectAdlib = new Adlib(options);
+    options["adlib"] = objectAdlib;
+    const thesaurusMapper = new ThesaurusMapper(options);
+    objectAdlib.getStream().pipe(thesaurusMapper).pipe(backend);
 }
--- controllers/discoveryController.js
+++ controllers/discoveryController.js
@@ -42,12 +42,26 @@ module.exports.getDiscoveryMetadata = async function(req, res) {
                 group: "database"
             });
             for (let d in databases) {
+                let institutionName = "";
+                let uitgevers = [];
+                if (institutions[i].institution === "adlib") {
+                    for (let ins in institutions) {
+                        if (institutions[ins].institution != "adlib") {
+                            institutionName += config[institutions[ins].institution].institutionName;
+                            if (ins < (institutions.length-1)) institutionName += ", "
+                            uitgevers.push(config[institutions[ins].institution].institutionURI);
+                        }
+                    }
+                } else {
+                    institutionName = config[institutions[i].institution].institutionName;
+                    uitgevers = config[institutions[i].institution].institutionURI;
+                }
                 md["heeftDataset"].push({
                     "@id": baseURI + 'dataset/' + institutions[i].institution + '/' +  md5(institutions[i].institution + databases[d].database),
                     "@type": "Dataset",
-                    "Dataset.titel": databases[d].database + " van " + config[institutions[i].institution].institutionName,
-                    "Dataset.beschrijving": "Event stream van de Adlib database '" + databases[d].database + "' van de instelling " + config[institutions[i].institution].institutionName,
-                    "Dataset.heeftUitgever": config[institutions[i].institution].institutionURI,
+                    "Dataset.titel": databases[d].database + " van " + institutionName,
+                    "Dataset.beschrijving": "Event stream van de Adlib database '" + databases[d].database + "' van de instelling: " + institutionName,
+                    "Dataset.heeftUitgever": uitgevers,
                     "heeftDistributie": {
                         "@type": "Distributie",
                         "toegangsURL": config.eventstream.protocol + '://' + config.eventstream.hostname + port + '/' + path + institutions[i].institution + '/' + databases[d].database,
